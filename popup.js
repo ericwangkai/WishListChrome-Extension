b54c8142cf731cb30d4ccb1ad80c9d9860111f33
ericwangkai <eric.wangkai@gmail.com>
2013-04-22 18:36:48 +0800

    update

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+WEB_ROOT_URL = "http://meteor.ericwangkai.c9.io";
+
 function actionClicked(tab) {
   document.getElementById("productDesc").value = tab.title;
 };
@@ -8,9 +10,12 @@ function showLoginForm(){
 };
 
 function showAddToForm(){
-	$("#p_welcome").append(localStorage.WISH_LIST_USEREMAIL);
+	$("#div_login").hide();
 	$("#div_addto").show();
-    $("#div_login").hide();
+	$("#p_welcome").append(localStorage.WISH_LIST_USEREMAIL);
+	chrome.windows.getCurrent(function(win) {
+			chrome.tabs.getSelected(win.id, actionClicked);
+	});
 };
 
 function onLoginSuccessfully(userEmail, data){
@@ -34,11 +39,8 @@ function clearToken(){
 };
 
 $(function () {
-	if(localStorage.isLogin === true){
+	if(localStorage.isLogin == "true"){
 		showAddToForm();
-		chrome.windows.getCurrent(function(win) {
-			chrome.tabs.getSelected(win.id, actionClicked);
-		});
 	}else{
 		showLoginForm();
 	}
@@ -49,11 +51,11 @@ $(function () {
 		
 		var email = $("#inputEmail").val();
 		var password = $("#inputPassword").val();
-		
+		var url = WEB_ROOT_URL + "/login";
 		var request = $.ajax({
 			type: "POST",
 			dataType: "json",
-			url: "http://meteor.ericwangkai.c9.io/login",
+			url: url,
 			data: { email: email, password: password }
 		}).done(function( data ) {
 			  onLoginSuccessfully(email, data);
@@ -62,10 +64,41 @@ $(function () {
 		});
 	});
 	
+	$("#addToForm").submit(function(event){
+		
+		event.preventDefault();
+		
+		var productDesc = $("#productDesc").val();
+		var productPrice = $("#productPrice").val();
+		var userId = localStorage.WISH_LIST_USERID;
+		var url = WEB_ROOT_URL + "/addToWishlist"
+		
+		
+		var request = $.ajax({
+			type: "POST",
+			dataType: "json",
+			url: url,
+			data: { productDesc: productDesc, productPrice: productPrice, userId: userId }
+		}).done(function( data ) {
+			  onAddToSuccess();
+		}).fail(function( jqxhr, textStatus, error ) {
+			  onAddToFail();
+		});
+		
+	});
+	
 	$("#aClearToken").click(function(){
 		clearToken();
 	});
 });
 
+function onAddToSuccess(){
+	alert(success);
+	//window.close();
+};
+
+function onAddToFail(){
+	alert(Fail);
+};
 
 
